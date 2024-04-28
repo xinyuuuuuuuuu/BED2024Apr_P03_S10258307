b18d323a75d292f88afc230a43a0f44d0920258b
xinyuuuuuuuuu <148407031+xinyuuuuuuuuu@users.noreply.github.com>
2024-04-28 22:18:44 +0800

    update wk_1 practical 2

--- wk_1/Practical_2/StaticFileExpress/app.js
+++ wk_1/Practical_2/StaticFileExpress/app.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const port = 3020;
 
-app.use(express.static(path.join('/Users/leong/OneDrive/Desktop/y2 sem1/BED/BED2024Apr_P03_S10258307/wk_1/Practical_2/public', 'public')))
+// using public folder at the root of the project
+app.use(express.static('public'));
 
-app.get('/', function(req,res, next) {
-    res.render('home.ejs');
-})
+// using image folder at the route 
+app.use('/images', express.static('images'));
 
-app.listen(port, function(err) {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log('Server listening on port', port);
-    }
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
 })
