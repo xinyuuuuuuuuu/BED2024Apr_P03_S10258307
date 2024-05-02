5eefabe2665e1ffced65cb139f1e21b683f5d978
xinyuuuuuuuuu <148407031+xinyuuuuuuuuu@users.noreply.github.com>
2024-05-02 09:54:02 +0800

    update wk_1 Practical 2

--- wk_1/Practical_2/StaticFileExpress/app.js
+++ wk_1/Practical_2/StaticFileExpress/app.js
@@ -5,8 +5,8 @@ const port = 3020;
 // using public folder at the root of the project
 app.use(express.static('public'));
 
-// using image folder at the route 
-app.use('/images', express.static('images'));
+// using the images folder at the route / images
+app.use('/images', express.static('images')); // '/images' - the route, images are gg to be pull from the images folder
 
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
